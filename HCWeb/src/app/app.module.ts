3ec1d3f7e693d743c483c17af0341143387303f6
DESKTOP-3BGEHE7\Henock <henockmamo54@gmail.com>
2020-09-07 01:35:54 +0900

    project created

---
+++ HCWeb/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PredictionComponent } from './prediction/prediction.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PredictionComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ HCWeb/src/app/prediction/prediction.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prediction',
+  templateUrl: './prediction.component.html',
+  styleUrls: ['./prediction.component.css']
+})
+export class PredictionComponent implements OnInit {
+
+  constructor() { }
+
+  msg:string ="fdsfadsfsaf"
+
+  ngOnInit(): void {
+  }
+
+}
