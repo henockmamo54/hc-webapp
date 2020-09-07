bb340b6f6413b5fee57f73d55d1a2552d72cb6c4
DESKTOP-3BGEHE7\Henock <henockmamo54@gmail.com>
2020-09-07 15:33:46 +0900

    chart format changed

--- HCWeb/src/app/prediction/prediction.component.ts
+++ HCWeb/src/app/prediction/prediction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 
 
@@ -13,19 +14,23 @@ import { Component, OnInit } from '@angular/core';
 export class PredictionComponent implements OnInit {
 
   constructor() { }
- 
-  FPG=150;
-  hbalc=6.8;
-  gammagtp=78;
-  bmi=15;
-  triglycerides=25;
-  age=28;
-  uricacid=5.5;
-  sex=0;
-  physicalactivity=5
-  drinking=2;
-  smoking=4;
-  familyhistory=1;
+
+  PieChart: any = [];
+
+
+
+  FPG = 150;
+  hbalc = 6.8;
+  gammagtp = 78;
+  bmi = 15;
+  triglycerides = 25;
+  age = 28;
+  uricacid = 5.5;
+  sex = 0;
+  physicalactivity = 5
+  drinking = 2;
+  smoking = 4;
+  familyhistory = 1;
 
   public barChartOptions = {
     scaleShowVerticalLines: false,
@@ -40,12 +45,61 @@ export class PredictionComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+
+
+    this.PieChart = new Chart('lineChart', {
+      type: 'pie',        
+      data: {
+        labels: ["Normal", "Prediabetes", "Diabetes"],
+        datasets: [{
+          label: '# of Votes',
+          data: [45, 35, 20],
+          backgroundColor: [
+            '#9CCC66',
+            '#27A69A',
+            '#465B65',
+          ],
+          borderColor: [
+            '#9CCC66',
+            '#27A69A',
+            '#465B65',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        title: {
+          text: "Next year status",
+          display: true
+        },
+        legend: {
+          display: true, 
+          position:"bottom"
+      }
+        // scales: {
+        //   yAxes: [{
+        //     ticks: {
+        //       beginAtZero: true
+        //     }
+        //   }]
+        // }
+      }
+    });
+
+
   }
 
-  onSelectionChange(){
+  onSelectionChange() {
 
     console.log("on change event")
 
+    this.PieChart.data.datasets[0].data = [19, 17, 13, 50, 20, 100, 115, 116, 119, 31, 1, 9];
+    this.PieChart.update()
   }
+
+
+
+
+
 
 }
