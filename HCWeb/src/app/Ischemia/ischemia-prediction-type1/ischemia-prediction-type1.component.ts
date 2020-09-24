672da0609b944218fe526f9bccedf8e930fbb125
DESKTOP-3BGEHE7\Henock <henockmamo54@gmail.com>
2020-09-24 13:49:23 +0900

    ischemia prediction added

--- HCWeb/src/app/Ischemia/ischemia-prediction-type1/ischemia-prediction-type1.component.ts
+++ HCWeb/src/app/Ischemia/ischemia-prediction-type1/ischemia-prediction-type1.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/user.model';
 
 @Component({
   selector: 'app-ischemia-prediction-type1',
@@ -7,9 +9,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IschemiaPredictionType1Component implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  isloading: boolean;
+  userFormatedValue: any;
+
+  status = "Normal"
+  public canvasWidth = 300;
+  public needleValue = 45
+  public centralLabel = '65% '
+  public name = ''
+  public bottomLabel = ''
+  public options = {
+    hasNeedle: false,
+    needleColor: 'gray',
+    needleUpdateSpeed: 1000,
+    arcColors: ['#4db6ac', 'lightgray'],
+    arcDelimiters: [73,],
+    rangeLabel: ['0', '100'],
+    needleStartValue: 96,
+  }
+
+
+  constructor(public httpClient: HttpClient) {
+
+    this.formatuserData();
+    this.loadHyperlipedemiaPrediction();
+  }
 
   ngOnInit(): void {
   }
+
+
+  onSelectionChange() {
+    this.formatuserData();
+    this.loadHyperlipedemiaPrediction();
+  }
+
+
+  loadHyperlipedemiaPrediction() {
+
+    this.isloading = true;
+    this.httpClient.post("http://127.0.0.1:5000/predictIschemiaNextYearClass", this.userFormatedValue,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        })
+      }).subscribe(((response) => {
+        var response = response;
+        console.log("response", response)
+        setTimeout(() => { this.afterDataReceived(response); }, 500);
+      }));
+
+
+  }
+
+  classValueLable = ["Negative", "Positive"];
+  afterDataReceived(response: Object) {
+    this.isloading = false;
+    var classvalue = response["Class value"][0]["CLASS"];
+    this.status = this.classValueLable[classvalue];
+    this.needleValue = 100 * response["Class probability"][0]["CLASS " + classvalue].toFixed(2);
+    this.centralLabel = 100 * response["Class probability"][0]["CLASS " + classvalue].toFixed(2) + "%";
+
+  }
+
+  formatuserData() {
+
+    this.userFormatedValue = {
+      "0": { 
+        "L101700": this.user.gammagtp,
+        "S000300": this.user.bmi, 
+        "L100700": this.user.uricacid,
+        "L103300": this.user.cardiacriskfactor,
+        "L103100": this.user.HDLcholesterol,
+        "L190300": this.user.RBC,
+        "L103000": this.user.triglycerides,
+        "L190900": this.user.RBCDistributionWidth,
+        "L504700": this.user.CEABowelDisease,
+        "AGE": this.user.age,
+        "SEX": this.user.sex,
+        "FIELD_33": this.user.smoking,
+        "FIELD_38": this.user.drinking,
+        "FIELD_15": this.user.diagnosedWithHighBloodPressure
+      }
+    };
+
+    return this.userFormatedValue;
+  }
+
 
 }
--- HCWeb/src/app/user.model.ts
+++ HCWeb/src/app/user.model.ts
@@ -1,32 +1,36 @@
 export class User {
 
-    FPG: number=110;
-    hbalc: number=4.5;
-    gammagtp: number=45;
-    bmi: number=15;
-    triglycerides: number=25;
-    age: number=45;
-    uricacid: number=5.5;
-    sex: number=0;
-    physicalactivity: number=5;
-    drinking: number=2;
-    smoking: number=4;
-    familyhistory: number=0;
+    FPG: number = 110;
+    hbalc: number = 4.5;
+    gammagtp: number = 45;
+    bmi: number = 15;
+    triglycerides: number = 25;
+    age: number = 45;
+    uricacid: number = 5.5;
+    sex: number = 0;
+    physicalactivity: number = 5;
+    drinking: number = 2;
+    smoking: number = 4;
+    familyhistory: number = 0;
 
-    creatinine: number=1;
-    serumGOT: number=16;
-    serumGPT: number=16;
-    alkphosphatse: number=48;
-    HDLcholesterol: number=73;
-    cardiacriskfactor: number=3.1;
-    BUNCREAratio: number=12.5;
-    WBC: number=3.64;
-    RBC: number=3.76;
-    hemoglobin: number=12.6;
-    height: number=167;
-    SBP: number=130;
-    DBP: number=80;
-    hct: number=12;  
+    creatinine: number = 1;
+    serumGOT: number = 16;
+    serumGPT: number = 16;
+    alkphosphatse: number = 48;
+    HDLcholesterol: number = 73;
+    cardiacriskfactor: number = 3.1;
+    BUNCREAratio: number = 12.5;
+    WBC: number = 3.64;
+    RBC: number = 3.76;
+    hemoglobin: number = 12.6;
+    height: number = 167;
+    SBP: number = 130;
+    DBP: number = 80;
+    hct: number = 12;
+
+    diagnosedWithHighBloodPressure: number = 0;
+    RBCDistributionWidth: number = 12;
+    CEABowelDisease = 12;
 
 
 }
